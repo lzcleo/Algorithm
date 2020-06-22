26f3ca3890cfe504249878fddf960969716af95e
luzhuochen <luzhuochen@meituan.com>
2020-06-22 09:29:51 +0800

    udpate

--- src/leetcode/Dyp/打家劫舍.java
+++ src/leetcode/Dyp/打家劫舍.java
@@ -1,10 +1,104 @@
 package leetcode.Dyp;
 
+import java.util.HashMap;
+
 /**
  * User: leo
  * Date: 2020/6/20
  * Time: 19:41
  */
 public class 打家劫舍 {
+    /**
+     *你是一个专业的小偷，计划偷窃沿街的房屋。每间房内都藏有一定的现金，影响你偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警
+     * 给定一个代表每个房屋存放金额的非负整数数组，计算你 不触动警报装置的情况下 ，一夜之内能够偷窃到的最高金额。
+     * 下面根据传参分为三种，规则相同
+     */
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        for (int i = 1; i < nums.length; i ++) {
+            if (i == 1) {
+                dp[i] = Math.max(dp[0], nums[i]);
+            } else {
+                dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+            }
+        }
+        return dp[nums.length - 1];
+    }
 
+    //优化空间复杂度
+    public int robSecond(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int length = nums.length;
+        if (length == 1) {
+            return nums[0];
+        }
+        int first = nums[0], second = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < length; i++) {
+            int temp = second;
+            second = Math.max(first + nums[i], second);
+            first = temp;
+        }
+        return second;
+    }
+
+}
+
+class 打家劫舍II {
+
+}
+
+class 打家劫舍III {
+    /**
+     * 首先是使用递归，对于每个节点，都会有两种选择，偷这个节点以及不偷这个节点，比较好理解，但是要注意的是避免空指针异常
+     */
+    public int rob(TreeNode root) {
+        if (root == null) return 0;
+        int money = root.val;
+        if (root.left != null) {
+            money += (rob(root.left.left) + rob(root.left.right));
+        }
+        if (root.right != null) {
+            money += (rob(root.right.left) + rob(root.right.right));
+        }
+        return Math.max(money, rob(root.left) + rob(root.right));
+    }
+    /**
+     * 使用记忆化提升效率
+     */
+    private HashMap<TreeNode, Integer> memo = new HashMap<>();
+    public int robSecond(TreeNode root) {
+        if (root == null) return 0;
+
+        if (memo.containsKey(root)) return memo.get(root);
+
+        int money = root.val;
+        if (root.left != null) {
+            money += (robSecond(root.left.left) + robSecond(root.left.right));
+        }
+        if (root.right != null) {
+            money += (robSecond(root.right.left) + robSecond(root.right.right));
+        }
+        int res = Math.max(money, robSecond(root.left) + robSecond(root.right));
+
+        memo.put(root, res);
+
+        return res;
+    }
+    /**
+     * 使用动态规划
+     */
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
 }
---
+++ src/leetcode/array/移动零.java
@@ -0,0 +1,50 @@
+package leetcode.array;
+
+/**
+ * User: leo
+ * Date: 2020/6/21
+ * Time: 16:34
+ * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+ * 输入: [0,1,0,3,12]
+ * 输出: [1,3,12,0,0]
+ */
+public class 移动零 {
+    //    我们创建两个指针i和j，第一次遍历的时候指针j用来记录当前有多少非0元素。即遍历的时候每遇到一个非0元素就将其往数组左边挪，第一次遍历完后，j指针的下标就指向了最后一个非0元素下标。
+    //    第二次遍历的时候，起始位置就从j开始到结束，将剩下的这段区域内的元素全部置为0
+    public void moveZeroes(int[] nums) {
+        if(nums==null) {
+            return;
+        }
+        //第一次遍历的时候，j指针记录非0的个数，只要是非0的统统都赋给nums[j]
+        int j = 0;
+        for(int i=0;i<nums.length;++i) {
+            if(nums[i]!=0) {
+                nums[j++] = nums[i];
+            }
+        }
+        //非0元素统计完了，剩下的都是0了
+        //所以第二次遍历把末尾的元素都赋为0即可
+        for(int i=j;i<nums.length;++i) {
+            nums[i] = 0;
+        }
+    }
+
+    //参考快速排序，进行交换操作，但是需要注意的是，题目没说不能有负数，不能直接使用partition，否则要是有负数0就在中间了
+    public void moveZeroesSecond(int[] nums)  {
+        int length;
+        if (nums == null || (length = nums.length) == 0) {
+            return;
+        }
+        int j = 0;
+        for (int i = 0; i < length; i++) {
+            if (nums[i] != 0) {
+                if (i > j) {// #1
+                    nums[j] = nums[i];
+                    nums[i] = 0;
+                }
+                j++;
+            }
+        }
+    }
+
+}
